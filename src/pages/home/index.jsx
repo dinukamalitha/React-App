da1e75f57879dff4bcc5632846d602e07ca8beff
dinukamalitha <dmcoder01@gmail.com>
2022-12-23 10:59:02 +0530

    Component Modified

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import {Component, Fragment} from "react";
 import React from "react";
 import Greeting from "../../components/home/greeting";
 import Counter from "../../components/home/counter";
+import {Button, Divider} from "@mui/material";
 
 class HomePage extends Component {
     constructor(props) {
@@ -12,7 +13,13 @@ class HomePage extends Component {
         return(
             <Fragment>
                 <Greeting name="World"/>
+                <Divider light />
                 <Counter/>
+                <Button variant="contained" onClick={()=>{
+                    console.log("Button Clicked!");
+                }}>
+                    Contained
+                </Button>
             </Fragment>
         )
     }
